1e03f0e7d5c3db3c84e63c0710d65045c8c5448b
NelidaSh <nelida.quispetacuri@gmail.com>
2018-01-16 12:55:21 -0500

    añadiendo efecto splash

---
+++ README.md
@@ -0,0 +1,18 @@
+#  FLASHBACK
+
+* Unidad: *Hackathon*
+***
+El presente proyecto consiste en ...
+
+## Objetivo
+***
+
+## Flujo de la Aplicación
+***
+
+## Autores
+***
+* Nelida Sheridan Quispe Tacuri.
+* Alejandra
+* Silvia
+* Ambar
---
+++ js/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApOvhgWv7K4RzFujEocOKbRD3V3xMr6Mk",
+    authDomain: "hackathon-72e7d.firebaseapp.com",
+    databaseURL: "https://hackathon-72e7d.firebaseio.com",
+    projectId: "hackathon-72e7d",
+    storageBucket: "hackathon-72e7d.appspot.com",
+    messagingSenderId: "121505655533"
+  };
+  firebase.initializeApp(config);
+});
