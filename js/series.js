5a21544a54ddab1a5b9e5b8fdd19e492bb2645e9
AlejandraCP <alejandracp710@gmail.com>
2018-01-19 03:33:37 -0500

    avance vista serie

--- js/series.js
+++ js/series.js
@@ -6,7 +6,7 @@
 function totalSeries() {
   var $seriesBox = $('.series-box');
   for (var i = 0; i < data.length; i++) {
-    $seriesBox.append('<div data-target="modal1" class="modal-trigger modal-box" data-titulo="' + data[i].titulo + '"><div class="col m4 s6"><p>' + data[i].titulo + ' ' + data[i].año + '</p></div><div><img class="responsive-img" src="' + data[i].imagen + '"></div></div>');
+    $seriesBox.append('<div data-target="modal1" class="modal-trigger modal-box col m5 offset-by-6 s3" data-titulo="' + data[i].titulo + '"><div class="col m4 s6"><p>' + data[i].titulo + ' ' + data[i].año + '</p></div><div><img class="responsive-img" src="' + data[i].imagen + '"></div></div>');
     console.log('funciona!!!');
   }
 }
